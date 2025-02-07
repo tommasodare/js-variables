25f2ba0a53598c86642e64c8ee4de78e8dcfd1e9
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-07 16:14:27 +0100

    Redone File One

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,25 +6,25 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const somma = 8 + 16;
+const somma = numOne + numTwo;
 console.log(somma); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
-const molt = 16*32;
+const molt = numTwo*numThree;
 console.log(molt); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
-const divisione = 32/8;
+const divisione = numThree/numOne;
 console.log(divisione); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const sottrazione = 32-8;
+const sottrazione = numThree-numOne;
 console.log(sottrazione); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const somma_2 = 8 + 16 + 32;
+const somma_2 = somma + numThree;
 console.log(somma_2); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = somma_2 / 8;
+const numBytes = somma_2 / numOne;
 console.log(numBytes); // Output: 7
